fe20021416e1c132287e89f965311b899d5cea59
Etienne Descamps <16402136+Etdescamps@users.noreply.github.com>
2018-07-09 11:33:49 +0200

    examples: fix rosenbrock example

--- examples/optim_rosenbrock.c
+++ examples/optim_rosenbrock.c
@@ -5,8 +5,8 @@ int fun_objfun(const double *x, double *y, int nD, int nY, int lambda, void *ptr
     double y0 = 0;
     const double *x0 = &x[i*nD];
     for(int j=0; j<nD-1; j++) {
-      double v = 10.0*(x0[j+1]-x0[j])*x0[j];
-      y0 += v*v+x0[j]-1;
+      double v = 10.0*(x0[j+1]-x0[j])*x0[j], w = x0[j]-1;
+      y0 += v*v+w*w;
     }
     y[i*nY] = y0;
   }

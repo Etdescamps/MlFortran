e40cef264396b371423a07692edc4e7f5c193b4a
Etienne Descamps <16402136+Etdescamps@users.noreply.github.com>
2018-07-13 09:22:05 +0200

    CMake: add tests to module

--- cmake/MlFortranConfig.cmake
+++ cmake/MlFortranConfig.cmake
@@ -4,5 +4,19 @@ set(ML_FORTRAN_LIB_DIR ${ML_FORTRAN_CM_DIR}/../../lib)
 include_directories(${ML_FORTRAN_INCLUDE_DIR})
 link_directories(${ML_FORTRAN_LIB_DIR})
 
+if(EXISTS "${ML_FORTRAN_LIB_DIR}/libmlfortran.so")
+  message("-- MlFortran library found")
+elseif(EXISTS "${ML_FORTRAN_LIB_DIR}/libmlfortran.dylib")
+  message("-- MlFortran library found")
+else()
+  message(SEND_ERROR "Error: MlFortran library not found")
+endif()
+
+if(EXISTS "${ML_FORTRAN_INCLUDE_DIR}/mlf_cintf.h")
+  message("-- MlFortran headers found")
+else()
+  message(SEND_ERROR "Error: MlFortran headers not found")
+endif()
+
 
 

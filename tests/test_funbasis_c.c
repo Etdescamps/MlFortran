b03c7266660398e5ab631a6ce22f6d10052cb80d
Etienne Descamps <16402136+Etdescamps@users.noreply.github.com>
2018-07-17 12:39:40 +0200

    test_funbasis_c: fix scale

--- tests/test_funbasis_c.c
+++ tests/test_funbasis_c.c
@@ -24,12 +24,12 @@ int main(int argc, char **argv) {
   mlf_init();
   MLF_OBJ *fobj = mlf_basisfunction(fBasis_invExp, NULL);
   for(int i=0; i<NG; i++) {
-    double vGi = ((double) i)/(NG-1.0);
+    double vGi = -2.0+6.0*((double) i)/(NG-1.0);
     double x = ((double) i) / ((double) NG-1);
     double w1 = 1.0*(1.0-x)+0.01*x;
     double w2 = 0.2*(1.0-x)+1.*x;
     for(int j=0; j<NR; j++) {
-      double vRj = 5.0*exp(-((double) j)/(NR-1.0)*log(2.0));
+      double vRj = 4.0*exp(((double) j)/(NR-1.0)*log(0.01));
       params[i][j][0]=vGi;
       params[i][j][1]=vRj;
       double y = ((double) j) / ((double) NR-1);
@@ -38,10 +38,10 @@ int main(int argc, char **argv) {
   }
   double alpha = 0.0;
   MLF_OBJ *fbasis = mlf_funBasisInit(fobj, 2, alpha, 0.0, 3.0, (double *) params, NR*NG, NPAR, 4096, (double *) weights);
-  MLF_OBJ *f2dgrid = mlf_2dgridModelInit(fbasis, 0.0, 4.0, 0.0, 4.0, NPAR, 1024, 1024);
+  MLF_OBJ *f2dgrid = mlf_2dgridModelInit(fbasis, -2.0, 4.0, 0.0, 4.0, NPAR, 1024, 1024);
   //MLF_OBJ *f2dgrid = mlf_2dgridModelInit(fbasis, 0.0, 4.0, 0.0, 4.0, NPAR, 2048, 2048);
   double Y[2];
-  for(Y[0] = 0.1; Y[0] < 4; Y[0] *= 1.5)
+  for(Y[0] = -2.0; Y[0] < 4.0; Y[0] += 0.3)
     for(Y[1] = 0.1; Y[1] < 3; Y[1] *= 1.4) {
       printf("Function %f %f:\n", Y[0], Y[1]);
       //mlf_getProj(fbasis, (double*) Y, (double*) W, 1, 2, NPAR);
